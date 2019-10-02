be88d7d73ea6459ee5bd6abf5caf76ee0d4f58c0
jayaprakashjp12 <48912744+jayaprakashjp12@users.noreply.github.com>
2019-10-02 13:53:59 +0530

    Update postmid2.cpp

--- postmid2.cpp
+++ postmid2.cpp
@@ -1,170 +1,135 @@
-#include <iostream>
-#include <stack>
-#include <stdlib.h>
-using namespace std;
-struct point{
-int x;int y;
-};
-point p0;
-int  swap(point &p1,point &p2){
-point temp=p1;
-p1=p2;
-p2=temp;
-}
-int dist(point p1,point p2)
-{
-return (p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y);}
-point nextToTop(stack<point> &S)
-{
-    point p = S.top();
-    S.pop();
-    point res = S.top();
-    S.push(p);
-    return res;
-}
-int orientation(point a,point b,point c){
-int dummy =(a.y-b.y)*(b.x-c.x)-(a.x-b.x)*(b.y-c.y);
-if(dummy==0){return 0;}
-return (dummy>0)?1:2;//1 for clock 2 for clockwise
-}
-int compare(const void *vp1, const void *vp2)
-{
-   point *p1 = (point *)vp1;
-   point *p2 = (point *)vp2;
+ #include <bits/stdc++.h> 
+using namespace std; 
+  
+struct Point 
+{ 
+    int x, y; 
+}; 
 
-   // Find orientation
-   int o = orientation(p0, *p1, *p2);
-   if (o == 0)
-     return (dist(p0, *p2) >= dist(p0, *p1))? -1 : 1;
+Point p0; 
+  
 
-   return (o == 2)? -1: 1;
-}
-void convexhull(point p[],int t)
-{
-  point minx;
-  minx.x=p[0].x;
-  minx.y=p[0].y;
-  int flag=0;
-  for(int i=0;i<t;i++)
-    {
-      if(p[i].x<minx.x)
-        {
-         minx.x=p[i].x;
-         minx.y=p[i].y;
-         flag=i;
-        }
-         if(p[i].x==minx.x&&p[i].y<minx.y)
-        {
-          minx.x=p[i].x;
-          minx.y=p[i].y;flag=i;}
-    }
+Point nextToTop(stack<Point> &S) 
+{ 
+    Point p = S.top(); 
+    S.pop(); 
+    Point res = S.top(); 
+    S.push(p); 
+    return res; 
+} 
+  
+int swap(Point &p1, Point &p2) 
+{ 
+    Point temp = p1; 
+    p1 = p2; 
+    p2 = temp; 
+} 
 
-  p0=minx;
-  swap(p[0],p[flag]);
+int distSq(Point p1, Point p2) 
+{ 
+    return (p1.x - p2.x)*(p1.x - p2.x) + 
+          (p1.y - p2.y)*(p1.y - p2.y); 
+} 
+  
 
-  qsort(&p[1],t-1,sizeof(point),compare);
-
-  int m = 1; // Initialize size of modified array
-   for (int i=1; i<t; i++)
-   {
-       // Keep removing i while angle of i and i+1 is same
-       // with respect to p0
-       while (i < t-1 && orientation(p0, p[i],
-                                    p[i+1]) == 0){
-          i++;}
-
-
-       p[m] = p[i];
-       m++;  // Update size of modified array
-   }
-
-   // If modified array of points has less than 3 points,
-   // convex hull is not possible
-   if (m < 3) return;
-
-   // Create an empty stack and push first three points
-   // to it.
-   stack<point> S;
-   S.push(p[0]);
-   S.push(p[1]);
-   S.push(p[2]);
-
-   // Process remaining n-3 points
-   for (int i = 3; i < m; i++)
-   {
-      // Keep removing top while the angle formed by
-      // points next-to-top, top, and points[i] makes
-      // a non-left turn
-      while (orientation(nextToTop(S), S.top(), p[i]) != 2)
-         S.pop();
-      S.push(p[i]);
-   }
-
-   // Now stack has the output points, print contents of stack
-   while (!S.empty())
-   {
-       point p = S.top();
-       cout << "(" << p.x << ", " << p.y <<")" << endl;
-       S.pop();
-   }
-
-
-/*  for(int i=1;i<t;i++){
-    while(i<t&&orientation(p0,p[i],p[i+1])){
-      i++;
-    }
-p[j]=p[i];
-j++;
-
-
-  }
-*/
+int orientation(Point p, Point q, Point r) 
+{ 
+    int val = (q.y - p.y) * (r.x - q.x) - 
+              (q.x - p.x) * (r.y - q.y); 
+  
+    if (val == 0) return 0;  // colinear 
+    return (val > 0)? 1: 2; // clock or counterclock wise 
+} 
  
+int compare(const void *vp1, const void *vp2) 
+{ 
+   Point *p1 = (Point *)vp1; 
+   Point *p2 = (Point *)vp2; 
+  
+   // Find orientation 
+   int o = orientation(p0, *p1, *p2); 
+   if (o == 0) 
+     return (distSq(p0, *p2) >= distSq(p0, *p1))? -1 : 1; 
+  
+   return (o == 2)? -1: 1; 
+} 
+  
 
+void convexHull(Point points[], int n) 
+{ 
+   // Find the bottommost point 
+   int ymin = points[0].y, min = 0; 
+   for (int i = 1; i < n; i++) 
+   { 
+     int y = points[i].y; 
+     if ((y < ymin) || (ymin == y && 
+         points[i].x < points[min].x)) 
+        ymin = points[i].y, min = i; 
+   } 
+  
+   swap(points[0], points[min]); 
+   p0 = points[0]; 
+   qsort(&points[1], n-1, sizeof(Point), compare);  
+   int m = 1;  
+   for (int i=1; i<n; i++) 
+   { 
+       // Keep removing i while angle of i and i+1 is same 
+       // with respect to p0 
+       while (i < n-1 && orientation(p0, points[i], 
+                                    points[i+1]) == 0) 
+          i++; 
+  
+  
+       points[m] = points[i]; 
+       m++;  // Update size of modified array 
+   } 
+   if (m < 3)
+   {
+       for(int i=0; i<m; i++)
+       {
+           cout<< points[i].x <<" " <<points[i].y <<endl;
+       }
+       return;
+   }
+ 
+   stack<Point> S; 
+   S.push(points[0]); 
+   S.push(points[1]); 
+   S.push(points[2]); 
+  
+   for (int i = 3; i < m; i++) 
+   { 
+      while (orientation(nextToTop(S), S.top(), points[i]) != 2) 
+         S.pop(); 
+      S.push(points[i]); 
+   }
 
+   vector <pair <int, int> > print; 
+  
+   while (!S.empty()) 
+   { 
+       Point p = S.top(); 
+       print.push_back({p.x, p.y});
+       S.pop(); 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //cout<<p0.x<<p0.y<<endl;
-  /*debuugging
-  cout<<flag;
-  cout<<p[0].x;
-    if(p0.x==p[0].x&&p0.y==p[0].y){
-   cout<<"hurray";*/
- }
-
-int  main()
+   } 
+   cout<< print.size() <<endl;
+   for(int i=0; i<print.size(); i++)
+   {
+       cout<< print[i].first <<" " <<print[i].second <<endl;
+   }
+} 
+  
+// Driver program to test above functions 
+int main() 
 {
-int t;
-cin>>t;
-point p[t];
-for(int i=0;i<t;i++)
-  {
-    cin>>p[i].x>>p[i].y;
-  }
-
-  convexhull(&p[0],t);
-
-
-
-
-
-return 0;
-
-
-}
+    int t;
+    cin>> t;
+    Point points[t];
+    for(int i=0; i<t; i++)
+    {
+        cin>> points[i].x >>points[i].y;
+    }
+    convexHull(points, t); 
+    return 0; 
+} 
