44a0156af90431e2f37d3fa49b8bc40363d64a38
jayaprakashjp12 <48912744+jayaprakashjp12@users.noreply.github.com>
2019-10-24 10:31:54 +0530

    Update postmid3_BLACK.cpp

--- postmid3_BLACK.cpp
+++ postmid3_BLACK.cpp
@@ -1,51 +1,53 @@
-#include<bits/stdc++.h>
-  using namespace std;
-
-  vector<vector<bool>> V;
-  int count(int i, int j,int c,int n,int m,vector<vector<int>> A)
-  {
-    c++;
-    //cout<<"("<<i<<","<<j<<") ";
-    V[i][j]=true;
-    if(j<m-1 && A[i][j]==A[i][j+1] && !V[i][j+1]) c=count(i,j+1,c,n,m,A);
-    if(j>0 && A[i][j]==A[i][j-1] && !V[i][j-1]) c=count(i,j-1,c,n,m,A);
-    if(i<n-1 && A[i][j]==A[i+1][j] && !V[i+1][j]) c=count(i+1,j,c,n,m,A);
-    if(i>0 && A[i][j]==A[i-1][j] && !V[i-1][j]) c=count(i-1,j,c,n,m,A);
-    return c;
-  }
-
-  int main()
-  {
-    int n,m;
-    cin>>n>>m;
-    vector<vector<int>> A;
+ #include<bits/stdc++.h>
+using namespace std;
+vector<vector<bool>> visited;
+int ans(int i, int j, vector<vector<int>>input,int n, int m,int val)
+    { 
+     val++;
+     visited[i][j]=true;
+     if(j<m-1&&!visited[i][j+1]&&input[i][j]==input[i][j+1]){val=ans(i,j+1,input,n,m,val);}
+     if(j>0&&!visited[i][j-1]&&input[i][j]==input[i][j-1]){val=ans(i,j-1,input,n,m,val);}
+     if(i<n-1&&!visited[i+1][j]&&input[i][j]==input[i+1][j]){val=ans(i+1,j,input,n,m,val);}
+     if(i>0&&!visited[i-1][j]&&input[i][j]==input[i-1][j]){val=ans(i-1,j,input,n,m,val);}
+     return val;
+    
+        
+    }
+int main()
+{
+ int n,m;
+ cin>>n>>m;
+ vector<vector<int>> input;
     vector<int> B;
     for(int i=0;i<m;i++) B.push_back(0);
-    for(int i=0;i<n;i++) A.push_back(B);
+    for(int i=0;i<n;i++) input.push_back(B);
     int k;
     cin>>k;
     for(int i=0;i<k;i++)
     {
       int x,y;
       cin>>x>>y;
-      A[x-1][y-1]=1;
+      input[x-1][y-1]=1;
     }
-    //vector<vector<bool>> V;
-    vector<bool> W;
+     vector<bool> W;
     for(int i=0;i<m;i++) W.push_back(false);
-    for(int i=0;i<n;i++) V.push_back(W);
+    for(int i=0;i<n;i++) visited.push_back(W);
     int max=0;
     for(int i=0;i<n;i++)
     {
       for(int j=0;j<m;j++)
       {
-        if(!V[i][j] && A[i][j]==1)
+        if(!visited[i][j] && input[i][j]==1)
         {
           //cout<<endl;
-          int cc=count(i,j,0,n,m,A);
+          int cc=ans(i,j,input,n,m,0);
           if(cc>max) max=cc;
         }
       }
     }
     cout<<max<<endl;
   }
+    
+    
+    
+    
