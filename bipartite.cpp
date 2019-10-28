aa03e6f0331887a67db0cf39629f7bbb1b998c08
jayaprakashjp12 <48912744+jayaprakashjp12@users.noreply.github.com>
2019-10-28 16:50:05 +0530

    Create bipartite.cpp
    still

---
+++ bipartite.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>>a;
+vector<int>col;
+int main()
+{
+ int n,m ;
+ cin>>n>>m;
+ int a[n+1][n+1]={ 0 };
+ vector<int>dummy;
+ for (int i=0;i<=n;i++)
+ {
+  dummy.push_back(0);
+ }
+ for (int i=0;i<=n;i++)
+ {
+    a..push_back(dummy);
+ }
+
+    for (int i=0;i<m;i++)
+    {
+      int a ,b;
+      cin>>c>>b;
+      a[c][b]=1;
+    }
+    for (int i=0;i<=n;i++)
+    {dummy.push_back(i);
+     col.push_back(0);
+    }
+int count=0;
+queue<int> a;
+queue<int>b;
+while(count!=n)
+{
+  a.enqueue(dummy[0]);
+  count++;
+  for(int i=1;i<=n;i++){ if(a[dummy[0]][i]==1){b.enqueue(i);count++}}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
